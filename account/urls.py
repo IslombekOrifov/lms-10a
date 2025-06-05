20c6af38c35afb448a0b0ffd67d86e1423f4f2a7
Islombek Orifov <islombekorifov0199@gmail.com>
2025-06-05 10:24:25 +0500

    user actions added

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import users_list
+from .views import users_list, user_detail, user_delete
 
 app_name = 'account'
 
 urlpatterns = [
     path('users/', users_list, name='users_list'),
+    path('users/<int:user_id>/', user_detail, name='user_detail'),
+    path('users/delete/<int:user_id>/', user_delete, name='user_delete'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 
 from .models import CustomUser, Role, RoleName
 
@@ -9,3 +10,19 @@ def users_list(request):
         'users': users,
     }
     return render(request, 'account/users-list.html', context)
+
+
+def user_detail(request, user_id):
+    user = CustomUser.objects.get(id=user_id)
+    context = {
+        'user': user,
+    }
+    return render(request, 'account/user-detail.html', context)
+
+
+def user_delete(request, user_id):
+    user = CustomUser.objects.get(id=user_id)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('account:users_list')   
+    raise Http404("User not found")
