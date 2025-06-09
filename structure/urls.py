2c19f5bbdc5bdc12214dd389b2d9a496d3c6e992
Islombek Orifov <islombekorifov0199@gmail.com>
2025-06-09 10:38:10 +0500

    academic year detail added

--- structure/urls.py
+++ structure/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import academic_year_list
+from .views import(
+    academic_year_list, academic_year_delete, academic_year_detail
+)
 
 app_name = 'structure'
 
 urlpatterns = [
     path('academic-year/', academic_year_list, name='academic_year_list'),
+    path('academic-year/<int:pk>/delete', academic_year_delete, name='academic_year_delete'),
+    path('academic-year/<int:id>/detail', academic_year_detail, name='academic_year_detail'),
 ]
--- structure/views.py
+++ structure/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import AcademicYear
 
@@ -9,3 +9,15 @@ def academic_year_list(request):
         'academic_years': academic_years
     }
     return render(request, 'structure/academic-year.html', context)
+
+
+def academic_year_delete(request, pk):
+    academic_years = AcademicYear.objects.get(id=pk).delete()
+    return redirect('structure:academic_year_list')
+
+def academic_year_detail(request, id):
+    academic_year = AcademicYear.objects.get(pk=id)
+    context = {
+        'academic_year': academic_year
+    }
+    return render(request, 'structure/academ-detail.html', context)
